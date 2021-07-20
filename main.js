2eb9a5ad6a44ac3f28237a58a1f2bf6cce6930d2
t-devl <79603858+t-devl@users.noreply.github.com>
2021-07-20 22:01:48 +0100

    Darken background when nav is open

--- main.js
+++ main.js
@@ -2,9 +2,11 @@ $(".nav__toggle").click(function () {
   if ($(".nav").hasClass("nav--active")) {
     $(".nav").removeClass("nav--active");
     $(".nav__toggle-icon").attr("src", "images/icon-hamburger.svg");
+    $("body").removeClass("no-scroll");
   } else {
     $(".nav").addClass("nav--active");
     $(".nav__toggle-icon").attr("src", "images/icon-close.svg");
+    $("body").addClass("no-scroll");
   }
 });
 
